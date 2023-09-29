8ba2bdc4629cb429bcf9d19528732a8edba6f660
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-09-29 15:53:47 +0700

    FIX STATUS BAR

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Text, Image } from "react-native";
+import { Button, Text, Image, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -12,19 +12,6 @@ import NotificationPage from "./src/screens/Notification/Page/NotificationPage";
 import CartPage from "./src/screens/Cart/Page/CartPage";
 
 const Stack = createNativeStackNavigator();
-
-// const HomeScreen = ({ navigation }) => {
-//   return (
-//     <Button
-//       title="Go to Jane's profile"
-//       onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-//     />
-//   );
-// };
-
-// const ProfileScreen = ({ navigation, route }) => {
-//   return <Text>This is {route.params.name}'s profile</Text>;
-// };
 
 export default function App() {
   return (
@@ -61,6 +48,11 @@ export default function App() {
           options={{ headerShown: false }}
         /> */}
       </Stack.Navigator>
+      <StatusBar
+        animated={true}
+        backgroundColor="#FFFFFF"
+        barStyle={"dark-content"}
+      />
     </NavigationContainer>
   );
 }
